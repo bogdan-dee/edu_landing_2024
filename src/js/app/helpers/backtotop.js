ae374a225d816d11940b0486d0190f56e10cd4c9
Bogdan Dee <bogdan.dee.global@gmail.com>
2024-11-07 04:25:40 +0200

    додано кнопку "Догори" на кожну сторінку

---
+++ src/js/app/helpers/backtotop.js
@@ -0,0 +1,14 @@
+export const backToTop = () => {
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 500, 'easeInOutExpo');
+        return false;
+    });
+}
